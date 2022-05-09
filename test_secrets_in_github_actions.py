a9eb7307721ae2a9cab2b1a1516ff005f64086fc
Ionut Budeanu <ionut.budeanu@paack.co>
2022-05-09 18:31:40 +0300

    Actions retry

--- test_secrets_in_github_actions.py
+++ test_secrets_in_github_actions.py
@@ -10,14 +10,14 @@ import pytest
 
 @pytest.fixture(scope="session")
 def my_user():
-    return f"{os.getenv('MY_USER')}"
+    return f"{os.environ['MY_USER']}"
 
 
 @pytest.fixture(scope="session")
 def my_password():
-    return f"{os.getenv('MY_PASS')}"
+    return f"{os.environ['MY_PASS']}"
 
 
 def test_credentials(my_user, my_password):
     print(f'\n>>>>> User: {my_user}, \n>>>>> Password: {my_password}')
-    assert "My" in my_user and "as" in my_password
+    assert "My" in my_user and "asswo" in my_password
