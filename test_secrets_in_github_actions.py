c9edf56a59b8b32a07c6834f0ee50638c9a83d9b
Ionut Budeanu <ionut.budeanu@paack.co>
2022-05-09 17:54:10 +0300

    Python Github Actions Demo

---
+++ test_secrets_in_github_actions.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import os
+import pytest
+
+
+@pytest.fixture(scope="session")
+def my_user():
+    return f"{os.getenv('MY_USER')}"
+
+
+@pytest.fixture(scope="session")
+def my_password():
+    return f"{os.getenv('MY_PASS')}"
+
+
+def test_credentials(my_user, my_password):
+    print(f'\n>>>>> User: {my_user}, \n>>>>> Password: {my_password}')
+    assert "My" in my_user and "as" in my_password
